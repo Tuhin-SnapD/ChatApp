eb601f38cbbbac8a31bd4566bcf60ff9b12eadc5
Tuhin-SnapD <67930437+Tuhin-SnapD@users.noreply.github.com>
2022-01-18 18:12:12 +0530

    Add files via upload

---
+++ node/index.js
@@ -0,0 +1,27 @@
+// Node server to control socketio connections
+
+const io = require('socket.io')(8000, {
+  cors: {
+    origin: '*',
+  }
+});
+
+const users = {};
+
+io.on('connection', socket => {
+  socket.on('new-user-joined', name => {
+    //console.log("New user", name)
+    users[socket.id] = name;
+    socket.broadcast.emit('user-joined', name)
+  });
+
+  socket.on('send', message => {
+    socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+  });
+
+  socket.on('disconnect', message => {
+    socket.broadcast.emit('left', users[socket.id]);
+    delete users[socket.id];
+
+  });
+})
