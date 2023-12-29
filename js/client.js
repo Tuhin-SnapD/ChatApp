f4c76708b22bdb8e5a2a2b099128f5606468a827
Tuhin Chakrabarty <tuhinchakrabarty14@gmail.com>
2023-12-30 01:54:46 +0530

    fixed UI

--- js/client.js
+++ js/client.js
@@ -1,40 +1,51 @@
 const socket = io('http://localhost:8000');
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+const audio = new Audio('ping.mp3');
 
-const form = document.getElementById('send-container')
-const messageInput = document.getElementById('messageInp')
-const messageContainer = document.querySelector(".container")
-var audio = new Audio('ping.mp3');
-
-const append = (message, position) => {
-    const messageElement = document.createElement('div')
+const appendMessage = (message, position) => {
+    const messageElement = document.createElement('div');
     messageElement.innerText = message;
-    messageElement.classList.add('message');
-    messageElement.classList.add(position);
+    messageElement.classList.add('message', position);
     messageContainer.append(messageElement);
-    if (position == 'left') {
+    if (position === 'left') {
         audio.play();
     }
-}
+};
+
+const sendMessage = (message) => {
+    appendMessage(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = '';
+};
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
-    append(`You: ${message}`, 'right');
-    socket.emit('send', message);
-    messageInput.value = ''
-})
+    sendMessage(message);
+});
 
-const name = prompt("Enter your name: ");
-socket.emit('new-user-joined', name);
+const handleNewUser = () => {
+    const name = prompt("Enter your name: ");
+    socket.emit('new-user-joined', name);
+};
 
-socket.on('user-joined', name => {
-    append(`${name} joined the chat`, 'right')
-})
+const handleUserJoined = (name) => {
+    appendMessage(`${name} joined the chat`, 'right');
+};
 
-socket.on('receive', data => {
-    append(`${data.name}: ${data.message}`, 'left')
-})
+const handleReceiveMessage = (data) => {
+    appendMessage(`${data.name}: ${data.message}`, 'left');
+};
 
-socket.on('left', name => {
-    append(`${name} left the chat`, 'right')
-})
+const handleUserLeft = (name) => {
+    appendMessage(`${name} left the chat`, 'right');
+};
+
+socket.on('user-joined', handleUserJoined);
+socket.on('receive', handleReceiveMessage);
+socket.on('left', handleUserLeft);
+
+// Initialize user on connection
+socket.on('connect', handleNewUser);
--- node/index.js
+++ node/index.js
@@ -1,27 +1,31 @@
-// Node server to control socketio connections
-
 const io = require('socket.io')(8000, {
   cors: {
     origin: '*',
-  }
+  },
 });
 
 const users = {};
 
-io.on('connection', socket => {
-  socket.on('new-user-joined', name => {
-    //console.log("New user", name)
-    users[socket.id] = name;
-    socket.broadcast.emit('user-joined', name)
-  });
+const handleNewUserJoined = (socket, name) => {
+  users[socket.id] = name;
+  socket.broadcast.emit('user-joined', name);
+};
 
-  socket.on('send', message => {
-    socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
-  });
+const handleSendMessage = (socket, message) => {
+  const senderName = users[socket.id];
+  socket.broadcast.emit('receive', { message, name: senderName });
+};
 
-  socket.on('disconnect', message => {
-    socket.broadcast.emit('left', users[socket.id]);
-    delete users[socket.id];
+const handleDisconnect = (socket) => {
+  const userName = users[socket.id];
+  socket.broadcast.emit('left', userName);
+  delete users[socket.id];
+};
 
-  });
-})
+io.on('connection', (socket) => {
+  socket.on('new-user-joined', handleNewUserJoined.bind(null, socket));
+
+  socket.on('send', handleSendMessage.bind(null, socket));
+
+  socket.on('disconnect', handleDisconnect.bind(null, socket));
+});
